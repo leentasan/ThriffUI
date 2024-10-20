4d1aaca3ea318d5d2ef888276c6d63567cb45d8f
dellafebialfian <dellafebialfian@mail.ugm.ac.id>
2024-10-20 21:20:22 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ThriffSignUp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadWelcomeView();
+        }
+
+        private void LoadWelcomeView()
+        {
+            var welcomeView = new View.WelcomeView();
+            this.Content = welcomeView; 
+        }
+
+        public void NavigateToLoginPage()
+        {
+            var loginView = new View.LoginView();
+            this.Content = loginView;
+        }
+    }
+}
+
