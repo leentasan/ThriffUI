1659bdab5e62c500c3d6ede877777f6ff0558784
dellafebialfian <dellafebialfian@mail.ugm.ac.id>
2024-11-26 11:57:30 +0700

    feat: add product and session cs

--- Model/Session.cs
+++ Model/Session.cs
@@ -10,5 +10,14 @@ namespace ThriffSignUp.Model
     {
         public static int LoggedInSellerId { get; set; }
         public static string SellerUsername { get; set; }
+        public static int BuyerId { get; set; }
+        public static string BuyerAddress { get; set; }
+        public static string Expedition { get; set; }
+        public static int Weight { get; set; }
+        public static int SellerId { get; set; }
+        public static int ProductId { get; set; }
+        public static double TotalAmount { get; set; }
+        public static List<int> ProductIds { get; set; } = new List<int>();
+
     }
 }
